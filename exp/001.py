08e668c389b0825a79afc52293a21b589aaaefb4
osuossu8 <osuosuossu18@gmail.com>
2022-05-28 16:01:17 +0000

    001

--- exp/001.py
+++ exp/001.py
@@ -162,7 +162,7 @@ for i, (trn_idx, val_idx) in tqdm(enumerate(kf.split(train, train["num_target"],
     train.loc[val_idx, "fold"] = i
 
 
-print(train.shape)
+print(train[features].shape)
 # print(train.head())
 
 """
@@ -228,5 +228,180 @@ oof, models = fit_lgbm(train[features], train[[f"target_{i}" for i in range(10)]
 """
 #print(oof.shape)
 #np.save(OUTPUT_DIR+'oof.npy', oof)
+
+
+class MLPDataset:
+    def __init__(self, X, y=None):
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, item):
+
+        features = self.X[item]
+
+        if self.y is not None:
+            targets = self.y[item]
+        
+            return {
+                'x': torch.tensor(features, dtype=torch.float32),
+                'y': torch.tensor(targets, dtype=torch.float32),
+            }
+          
+        else:
+            return {
+                'x': torch.tensor(features, dtype=torch.float32),
+            }  
+
+
+class MetricMeter(object):
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.y_true = []
+        self.y_pred = []
+    
+    def update(self, y_true, y_pred):
+        self.y_true.extend(y_true.cpu().detach().numpy().tolist())
+        self.y_pred.extend(y_pred.cpu().detach().numpy().tolist())
+
+    @property
+    def avg(self):
+        self.score = f1_score(np.array(self.y_true), np.array(self.y_pred) > 0.5, average="micro") # calc_loss(self.y_true, self.y_pred)
+       
+        return {
+            "score" : self.score,
+        }
+
+
+class MLP(nn.Module):
+    def __init__(self, len_features):
+        super(MLP, self).__init__()    
+
+        self.head = nn.Sequential(
+            nn.Linear(len_features, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, features):
+        # bs, len_features
+        output = self.head(features)
+        return output
+
+
+def loss_fn(logits, targets):
+    loss_fct = nn.BCEWithLogitsLoss(reduction='mean')
+    loss = loss_fct(logits, targets)
+    return loss
+
+
+def train_fn(model, data_loader, device, optimizer, scheduler):
+    model.train()
+    losses = AverageMeter()
+    scores = MetricMeter()
+    tk0 = tqdm(data_loader, total=len(data_loader))
+    
+    for data in tk0:
+        optimizer.zero_grad()
+        inputs = data['x'].to(device)
+        targets = data['y'].to(device)
+        outputs = model(inputs)
+        loss = loss_fn(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        losses.update(loss.item(), inputs.size(0))
+        scores.update(targets, outputs)
+        tk0.set_postfix(loss=losses.avg)
+    return scores.avg, losses.avg
+
+
+def valid_fn(model, data_loader, device):
+    model.eval()
+    losses = AverageMeter()
+    scores = MetricMeter()
+    tk0 = tqdm(data_loader, total=len(data_loader))
+    valid_preds = []
+    with torch.no_grad():
+        for data in tk0:
+            inputs = data['x'].to(device)
+            targets = data['y'].to(device)
+            outputs = model(inputs)
+            loss = loss_fn(outputs, targets)
+
+            losses.update(loss.item(), inputs.size(0))
+            scores.update(targets, outputs)
+            tk0.set_postfix(loss=losses.avg)
+    return scores.avg, losses.avg
+
+
+def run_one_fold(fold, df, features):
+    trn_df = df[df.fold != fold].reset_index(drop=True)
+    val_df = df[df.fold == fold].reset_index(drop=True)
+
+    train_dataset = MLPDataset(X=trn_df[features].values, y=trn_df[[f"target_{i}" for i in range(10)]].values)
+    train_loader = torch.utils.data.DataLoader(
+                   train_dataset, shuffle=True,
+                   batch_size=32,
+                   num_workers=0, pin_memory=True)
+
+    val_dataset = MLPDataset(X=val_df[features].values, y=val_df[[f"target_{i}" for i in range(10)]].values)
+    val_loader = torch.utils.data.DataLoader(
+                 val_dataset, shuffle=False,
+                 batch_size=32,
+                 num_workers=0, pin_memory=True)
+
+    del train_dataset, val_dataset
+    gc.collect()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = MLP(df[features].shape[1])
+    model = model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=1e-6)
+
+    patience = 5
+    p = 0
+    min_loss = 999
+    best_score = 999
+
+    for epoch in range(1, 100 + 1):
+
+        print("Starting {} epoch...".format(epoch))
+
+        start_time = time.time()
+
+        train_avg, train_loss = train_fn(model, train_loader, device, optimizer, scheduler)
+        valid_avg, valid_loss = valid_fn(model, val_loader, device)
+        scheduler.step()
+
+        elapsed = time.time() - start_time
+
+        print(f'Epoch {epoch+1} - avg_train_loss: {train_loss:.5f}  avg_val_loss: {valid_loss:.5f}  time: {elapsed:.0f}s')
+        print(f"Epoch {epoch+1} - train_rmse:{train_avg['score']:0.5f}  valid_rmse:{valid_avg['score']:0.5f}")
+
+        if valid_avg['score'] < best_score:
+            print(f">>>>>>>> Model Improved From {best_score} ----> {valid_avg['score']}")
+            torch.save(model.state_dict(), f'fold-{fold}.bin')
+            best_score = valid_avg['score']
+            p = 0
+
+        p += 1
+        if p > patience:
+            print(f'Early Stopping')
+            break
+
+
+for fold in range(5):
+    print("Starting fold {} ...".format(fold))
+
+    run_one_fold(fold, train, features)
 
 print('finished')
