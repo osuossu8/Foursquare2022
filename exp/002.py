e90994596d2db2eabd10d25b23f82b81f45a6056
osuossu8 <osuosuossu18@gmail.com>
2022-05-28 16:39:49 +0000

    002

---
+++ exp/002.py
@@ -0,0 +1,174 @@
+import os
+import gc
+import re
+import sys
+sys.path.append("/root/workspace/Foursquare2022")
+import json
+import time
+import math
+import string
+import pickle
+import random
+import joblib
+import itertools
+import warnings
+warnings.filterwarnings("ignore")
+
+import librosa
+import scipy as sp
+import numpy as np
+import pandas as pd
+import soundfile as sf
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+from tqdm.auto import tqdm
+
+from contextlib import contextmanager
+from pathlib import Path
+from typing import List
+from typing import Optional
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.model_selection import StratifiedKFold, GroupKFold, KFold
+
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+import torch.nn.functional as F
+from torch.optim import Adam, SGD, AdamW
+from torch.utils.data import DataLoader, Dataset
+
+import transformers
+
+
+from src.machine_learning_util import set_seed, set_device, init_logger, AverageMeter, to_pickle, unpickle
+
+
+class CFG:
+    EXP_ID = '002'
+    seed = 71
+    epochs = 5
+    folds = [0, 1, 2, 3, 4]
+    N_FOLDS = 5
+    LR = 1e-3
+    ETA_MIN = 1e-6
+    WEIGHT_DECAY = 1e-6
+    train_bs = 16
+    valid_bs = 32
+    EARLY_STOPPING = True
+    DEBUG = False # True
+    target = "point_of_interest"
+    n_neighbors = 10
+    n_splits = 3
+
+
+import os
+
+OUTPUT_DIR = f'output/{CFG.EXP_ID}/'
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+   
+
+set_seed(CFG.seed)
+device = set_device()
+logger = init_logger(log_file='log/' + f"{CFG.EXP_ID}.log")
+
+
+
+train = pd.read_csv('input/train_with_near_candidate_target.csv')
+
+del train[[f"target_{i}" for i in range(10)]]; gc.collect()
+
+
+for i in range(CFG.n_neighbors):
+    train.loc[train[CFG.target]==train[f"near_target_{i}"], "target"] = i
+
+
+import Levenshtein
+import difflib
+from requests import get
+import multiprocessing
+import joblib
+
+
+distance_features = unpickle('features/distance_features.pkl')
+
+print(distance_features.shape)
+print(distance_features.head(1))
+
+
+train = train[[CFG.target, "target", "id"] + [f"near_id_{i}" for i in range(CFG.n_neighbors)]]
+train = pd.concat([train, distance_features], 1)
+
+train.reset_index(drop=True, inplace=True)
+
+kf = StratifiedKFold(n_splits=CFG.n_splits, shuffle=True, random_state=CFG.seed)
+for i, (trn_idx, val_idx) in tqdm(enumerate(kf.split(train, train["target"], train["target"]))):
+    train.loc[val_idx, "fold"] = i
+
+
+print(train[features].shape)
+# print(train.head())
+
+
+import lightgbm as lgbm
+
+def fit_lgbm(X, y, params=None, es_rounds=20, seed=42, N_SPLITS=5,
+             n_class=None, model_dir=None, folds=None):
+    models = []
+    oof = np.zeros((len(y), n_class), dtype=np.float64)
+
+    for i in tqdm(range(CFG.n_splits)):
+        print(f"== fold {i} ==")
+        trn_idx = folds!=i
+        val_idx = folds==i
+        X_train, y_train = X[trn_idx], y.iloc[trn_idx]
+        X_valid, y_valid = X.iloc[val_idx], y.iloc[val_idx]
+
+        if model_dir is None:
+            model = lgbm.LGBMClassifier(**params)
+            model.fit(
+                X_train, y_train,
+                eval_set=[(X_valid, y_valid)],
+                early_stopping_rounds=es_rounds,
+                eval_metric='logloss',
+    #             verbose=-1)
+                verbose=50)
+        else:
+            with open(f'{model_dir}/lgbm_fold{i}.pkl', 'rb') as f:
+                model = pickle.load(f)
+
+        pred = model.predict_proba(X_valid)
+        oof[val_idx] = pred
+        models.append(model)
+
+        file = OUTPUT_DIR+f'lgbm_fold{i}.pkl'
+        pickle.dump(model, open(file, 'wb'))
+        print()
+
+    cv = (oof.argmax(axis=-1) == y).mean()
+    print(f"CV-accuracy: {cv}")
+    return oof, models
+
+
+params = {
+    'objective': "logloss",
+    'learning_rate': 0.2,
+    'reg_alpha': 0.1,
+    'reg_lambda': 0.1,
+    'random_state': 42,
+
+    'max_depth': 7,
+    'num_leaves': 35,
+    'n_estimators': 1000000,
+    "colsample_bytree": 0.9,
+}
+
+oof, models = fit_lgbm(train[features], train["target"].astype(int),
+                       params=params, n_class=int(train["num_target"].max() + 1),
+                       N_SPLITS=CFG.n_splits, folds=train["fold"].values)
+
+print(oof.shape)
+np.save(OUTPUT_DIR+'oof.npy', oof)
+
