32f2c1553f769fb6e7839e7b55e157faa2982568
osuossu8 <osuosuossu18@gmail.com>
2022-06-07 19:31:24 +0000

    src/create_candidate_v3.py

--- src/create_candidate_v3.py
+++ src/create_candidate_v3.py
@@ -78,13 +78,13 @@ print(train["set"].value_counts())
 NUM_NEIGHBOR = 20
 SEED = 2022
 THRESHOLD = 0.5
-NUM_SPLIT = 10
-feat_columns = ['dist', 'name', 'address', 'city', 
+NUM_SPLIT = 5
+feat_columns = ['name', 'address', 'city', 
             'state', 'zip', 'url', 
            'phone', 'categories', 'country']
 vec_columns = ['name', 'categories', 'address', 
                'state', 'url', 'country']
-rec_columns = ['name', 'address', 'categories', 'address', 'phone']
+
 
 def seed_everything(seed):
     random.seed(seed)
@@ -131,51 +131,6 @@ def vectorized_haversine(lats1, lats2, longs1, longs2):
     d = radius * c
     return d
 
-def recall_simple(df):
-    threshold = 2
-
-    val2id_d = {}
-    for col in rec_columns:
-        temp_df = df[['id', col]]
-        temp_df[col] = temp_df[col].str.lower()
-        val2id = temp_df.groupby(col)['id'].apply(set).to_dict()
-        val2id_d[col] = val2id
-        del val2id
-
-    cus_ids = []
-    match_ids = []
-    for vals in tqdm(df[rec_columns + ['id']].fillna('null').values):
-        cus_id = vals[-1]
-        match_id = []
-
-        rec_match_count = []
-        for i in range(len(rec_columns)):
-            col = rec_columns[i]
-
-            if vals[i] != 'null':
-                rec_match_count += list(val2id_d[col][vals[i].lower()])
-        rec_match_count = dict(Counter(rec_match_count))
-
-        for k, v in rec_match_count.items():
-            if v > threshold:
-                match_id.append(k)
-
-        cus_ids += [cus_id] * len(match_id)
-        match_ids += match_id
-
-    train_df = pd.DataFrame()
-    train_df['id'] = cus_ids
-    train_df['match_id'] = match_ids
-    train_df = train_df.drop_duplicates()
-    del cus_ids, match_ids
-
-    num_data = len(train_df)
-    num_data_per_id = num_data / train_df['id'].nunique()
-    print('Num of data: %s' % num_data)
-    print('Num of data per id: %s' % num_data_per_id)
-
-    return train_df
-
 
 def recall_knn(df, Neighbors = 10):
     print('Start knn grouped by country')
@@ -188,63 +143,50 @@ def recall_knn(df, Neighbors = 10):
                                     metric = 'haversine',
                                     n_jobs = -1)
         knn.fit(country_df[['latitude','longitude']], country_df.index)
-        dists, nears = knn.kneighbors(country_df[['latitude', 'longitude']],
+        dists, nears = knn.kneighbors(country_df[['latitude', 'longitude']], 
                                         return_distance = True)
 
-        for k in range(neighbors):
+        for k in range(neighbors):            
             cur_df = country_df[['id']]
             cur_df['match_id'] = country_df['id'].values[nears[:, k]]
             cur_df['kdist_country'] = dists[:, k]
             cur_df['kneighbors_country'] = k
-
+            
             train_df_country.append(cur_df)
     train_df_country = pd.concat(train_df_country)
-
+    
     print('Start knn')
     train_df = []
     knn = NearestNeighbors(n_neighbors = Neighbors)
     knn.fit(df[['latitude','longitude']], df.index)
     dists, nears = knn.kneighbors(df[['latitude','longitude']])
-
-    for k in range(Neighbors):
+    
+    for k in range(Neighbors):            
         cur_df = df[['id']]
         cur_df['match_id'] = df['id'].values[nears[:, k]]
         cur_df['kdist'] = dists[:, k]
         cur_df['kneighbors'] = k
         train_df.append(cur_df)
-
+    
     train_df = pd.concat(train_df)
     train_df = train_df.merge(train_df_country,
                                  on = ['id', 'match_id'],
                                  how = 'outer')
     del train_df_country
-
+    
     return train_df
 
 
 def add_features(df):
     for col in tqdm(feat_columns):
-        if col == 'dist':
-            lat = data.loc[df['id']]['latitude'].values
-            match_lat = data.loc[df['match_id']]['latitude'].values
-            lon = data.loc[df['id']]['longitude'].values
-            match_lon = data.loc[df['match_id']]['longitude'].values
-            df['latdiff'] = (lat - match_lat)
-            df['londiff'] = (lon - match_lon)
-            df['manhattan'] = manhattan(lat, lon, match_lat, match_lon)
-            df['euclidean'] = (df['latdiff'] ** 2 + df['londiff'] ** 2) ** 0.5
-            df['haversine'] = vectorized_haversine(lat, match_lat, lon, match_lon)
-            continue
-
-        col_values = data.loc[df['id']][col].values.astype(str)
-        matcol_values = data.loc[df['match_id']][col].values.astype(str)
-
         if col in vec_columns:
             tv_fit = tfidf_d[col]
             indexs = [id2index_d[i] for i in df['id']]
             match_indexs = [id2index_d[i] for i in df['match_id']]
-            df[f'{col}_sim'] = tv_fit[indexs].multiply(tv_fit[match_indexs]).\
-                                            sum(axis = 1).A.ravel()
+            df[f'{col}_sim'] = np.array(tv_fit[indexs].multiply(tv_fit[match_indexs]).sum(axis = 1)).ravel()
+
+        col_values = data.loc[df['id']][col].values.astype(str)
+        matcol_values = data.loc[df['match_id']][col].values.astype(str)
 
         geshs = []
         levens = []
@@ -270,8 +212,7 @@ def add_features(df):
         if col not in ['phone', 'zip']:
             df[f'{col}_len'] = list(map(len, col_values))
             df[f'match_{col}_len'] = list(map(len, matcol_values))
-            df[f'{col}_len_diff'] = np.abs(df[f'{col}_len'] - df[f'match_{col}_len']) /\
-                                        df[f'{col}_len']
+            df[f'{col}_len_diff'] = np.abs(df[f'{col}_len'] - df[f'match_{col}_len'])
             df[f'{col}_nleven'] = df[f'{col}_leven'] / \
                                     df[[f'{col}_len', f'match_{col}_len']].max(axis = 1)
 
@@ -285,52 +226,65 @@ def add_features(df):
     return df
 
 
-# special process
-def get_lower(x):
-    try:
-        return x.lower()
-    except:
-        return x
+id2index_d = dict(zip(train['id'].values, train.index))
 
+tfidf_d = {}
+for col in vec_columns:
+    tfidf = TfidfVectorizer()
+    tv_fit = tfidf.fit_transform(train[col].fillna('nan'))
+    tfidf_d[col] = tv_fit
 
-df_train = []
-for data in [train[train["set"]==0], train[train["set"]==1]]:
-    for col in data.columns:
-        if data[col].dtype == object and col != 'id':
-            data[col] = data[col].apply(get_lower)
-
-    id2index_d = dict(zip(data['id'].values, data.index))
-
-    tfidf_d = {}
-    for col in vec_columns:
-        tfidf = TfidfVectorizer()
-        tv_fit = tfidf.fit_transform(data[col].fillna('nan'))
-        tfidf_d[col] = tv_fit
-
-    train_data_simple = recall_simple(data)
-    train_data = recall_knn(data, NUM_NEIGHBOR)
-
-    print('train data by knn: %s' % len(train_data))
-    train_data = train_data.merge(train_data_simple,
-                                 on = ['id', 'match_id'],
-                                 how = 'outer')
-    del train_data_simple; gc.collect()
-    df_train.append(train_data)
-    del train_data; gc.collect()
-
-df_train = pd.concat(df_train, 0).reset_index(drop=True) 
+train_data = recall_knn(train, NUM_NEIGHBOR)
 
 id2poi = dict(zip(train['id'].values, train['point_of_interest'].values))
 
-df_train['target'] = (df_train['id'].map(id2poi) == df_train['match_id'].map(id2poi)).astype(int)
+train_data['target'] = (train_data['id'].map(id2poi) == train_data['match_id'].map(id2poi)).astype(int)
 
 
-print(df_train.shape)
-print(df_train['target'].value_counts())
+print(train_data.shape)
+print(train_data['target'].value_counts())
 
-#df_train = df_train[df_train['id']!=df_train['match_id']].reset_index(drop=True)
-#print(df_train.shape)
-#print(df_train['target'].value_counts())
+
+train = train.set_index('id')
+
+
+## Prediction
+count = 0
+start_row = 0
+pred_df = pd.DataFrame()
+unique_id = train_data['id'].unique().tolist()
+num_split_id = len(unique_id) // NUM_SPLIT
+for k in tqdm(range(1, NUM_SPLIT + 1)):
+    print('Current split: %s' % k)
+    end_row = start_row + num_split_id
+    if k < NUM_SPLIT:
+        cur_id = unique_id[start_row : end_row]
+        cur_data = train_data[train_data['id'].isin(cur_id)]
+    else:
+        cur_id = unique_id[start_row: ]
+        cur_data = train_data[train_data['id'].isin(cur_id)]
+
+    # add features & model prediction
+    cur_data = add_features(cur_data)
+    #cur_data_cat = Pool(cur_data[TRAIN_FEATURES])
+    #for fold in range(5):
+    #    with open(f'../input/foursquare-data/007_cat_gpu_baseline_{fold}.pkl','rb') as f:
+    #        cat_model = pickle.load(f)
+
+    #    cur_data['pred'] = cat_model.predict(cur_data_cat)/5
+    #cur_pred_df = cur_data[cur_data['pred'] > 0][['id', 'match_id']]
+    #pred_df = pd.concat([pred_df, cur_pred_df])
+
+    df_train.append(cur_data)
+
+    start_row = end_row
+    count += len(cur_data)
+
+    del cur_data, cur_pred_df
+    gc.collect()
+print(count)
+
+df_train = pd.concat(df_train, 0).reset_index(drop=True)
 
 df_train.to_csv('input/train_pairs.csv', index=False)
 
