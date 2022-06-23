85ab84b5e6385412c2d003b51f5a9e6b3b2c9366
osuossu8 <osuosuossu18@gmail.com>
2022-06-23 08:18:24 +0000

    src/downsampling_with_037_oof.py

---
+++ src/downsampling_with_037_oof.py
@@ -0,0 +1,260 @@
+import os
+import gc
+import re
+import sys
+sys.path.append("/root/workspace/Foursquare2022")
+import json
+import time
+import math
+import string
+import pickle
+import random
+import joblib
+import itertools
+import warnings
+warnings.filterwarnings("ignore")
+
+import librosa
+import scipy as sp
+import numpy as np
+import pandas as pd
+import soundfile as sf
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+from tqdm.auto import tqdm
+
+from contextlib import contextmanager
+from pathlib import Path
+from typing import List
+from typing import Optional
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.model_selection import StratifiedKFold, GroupKFold, KFold, StratifiedGroupKFold
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+import torch.nn.functional as F
+from torch.optim import Adam, SGD, AdamW
+from torch.utils.data import DataLoader, Dataset
+from torch.cuda.amp import autocast, GradScaler
+
+import transformers
+from transformers import AutoConfig, AutoModel, AutoModelForMaskedLM, AutoTokenizer
+
+from src.machine_learning_util import set_seed, set_device, init_logger, AverageMeter, to_pickle, unpickle
+
+
+# https://www.kaggle.com/code/columbia2131/foursquare-iou-metrics
+def get_id2poi(input_df: pd.DataFrame) -> dict:
+    return dict(zip(input_df['id'], input_df['point_of_interest']))
+
+def get_poi2ids(input_df: pd.DataFrame) -> dict:
+    return input_df.groupby('point_of_interest')['id'].apply(set).to_dict()
+
+def get_score(input_df: pd.DataFrame):
+    scores = []
+    for id_str, matches in zip(input_df['id'].to_numpy(), input_df['matches'].to_numpy()):
+        targets = poi2ids[id2poi[id_str]]
+        preds = set(matches.split())
+        score = len((targets & preds)) / len((targets | preds))
+        scores.append(score)
+    scores = np.array(scores)
+    return scores.mean()
+
+
+def post_process(df):
+    id2match = dict(zip(df['id'].values, df['matches'].str.split()))
+
+    for base, match in df[['id', 'matches']].values:
+        match = match.split()
+        if len(match) == 1:
+            continue
+
+        for m in match:
+            if base not in id2match[m]:
+                id2match[m].append(base)
+    df['matches'] = df['id'].map(id2match).map(' '.join)
+    return df
+
+
+def reduce_mem_usage(df):
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
+
+
+class CFG:
+    EXP_ID = '037'
+    seed = 71
+    epochs = 3
+    LR = 1e-3
+    ETA_MIN = 1e-6
+    WEIGHT_DECAY = 1e-6
+    train_bs = 16
+    valid_bs = 32
+    EARLY_STOPPING = True
+    DEBUG = False # True
+    target = "point_of_interest"
+    n_neighbors = 10
+    n_splits = 3
+    apex = True
+    #model_name = 'xlm-roberta-base'
+    #tokenizer = AutoTokenizer.from_pretrained(model_name)
+    #max_len = 180
+
+
+NUM_NEIGHBOR = 20
+SEED = 2022
+THRESHOLD = 0.5
+NUM_SPLIT = 5
+TRAIN_FEATURES = ['kdist',
+                'kneighbors',
+                'kdist_country',
+                'kneighbors_country',
+                'name_sim',
+                'name_gesh',
+                'name_leven',
+                'name_jaro',
+                'name_lcs',
+                'name_len_diff',
+                'name_nleven',
+                'name_nlcsk',
+                'name_nlcs',
+                'address_sim',
+                'address_gesh',
+                'address_leven',
+                'address_jaro',
+                'address_lcs',
+                'address_len_diff',
+                'address_nleven',
+                'address_nlcsk',
+                'address_nlcs',
+                'city_gesh',
+                'city_leven',
+                'city_jaro',
+                'city_lcs',
+                'city_len_diff',
+                'city_nleven',
+                'city_nlcsk',
+                'city_nlcs',
+                'state_sim',
+                'state_gesh',
+                'state_leven',
+                'state_jaro',
+                'state_lcs',
+                'state_len_diff',
+                'state_nleven',
+                'state_nlcsk',
+                'state_nlcs',
+                'zip_gesh',
+                'zip_leven',
+                'zip_jaro',
+                'zip_lcs',
+                'url_sim',
+                'url_gesh',
+                'url_leven',
+                'url_jaro',
+                'url_lcs',
+                'url_len_diff',
+                'url_nleven',
+                'url_nlcsk',
+                'url_nlcs',
+                'phone_gesh',
+                'phone_leven',
+                'phone_jaro',
+                'phone_lcs',
+                'categories_sim',
+                'categories_gesh',
+                'categories_leven',
+                'categories_jaro',
+                'categories_lcs',
+                'categories_len_diff',
+                'categories_nleven',
+                'categories_nlcsk',
+                'categories_nlcs',
+                'country_sim',
+                'country_gesh',
+                'country_leven',
+                'country_nleven',
+                
+                'text_1',
+                'text_2',
+]
+
+
+import os
+
+OUTPUT_DIR = f'output/{CFG.EXP_ID}/'
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+   
+
+set_seed(CFG.seed)
+device = set_device()
+logger = init_logger(log_file='log/' + f"{CFG.EXP_ID}.log")
+
+print('load data')
+train1 = pd.read_csv('input/train_data_candidate_25_1.csv')
+print(train1['label'].value_counts())
+train2 = pd.read_csv('input/train_data_candidate_25_2.csv')
+print(train2['label'].value_counts())
+train3 = pd.read_csv('input/train_data_candidate_25_3.csv')
+print(train3['label'].value_counts())
+train4 = pd.read_csv('input/train_data_candidate_25_4.csv')
+print(train4['label'].value_counts())
+train5 = pd.read_csv('input/train_data_candidate_25_5.csv')
+print(train5['label'].value_counts())
+
+train = pd.concat([
+    train1, train2, train3, train4, train5
+], 0).reset_index(drop=True)
+
+del train1, train2, train3, train4, train5; gc.collect()
+
+train['oof'] = np.load(OUTPUT_DIR+'oof.npy')
+
+print(train.shape)
+print(train['label'].value_counts())
+
+train = train[(train['id']!=train['match_id'])&(train['oof']>0.005)].reset_index(drop=True)
+train = reduce_mem_usage(train)
+
+print(train.shape)
+print(train['label'].value_counts())
+
+print(train[['id', 'match_id', 'oof']])
+
+train.to_csv('input/downsampled_with_oof_037_train_data.csv', index=False)
