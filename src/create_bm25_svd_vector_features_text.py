38b7eb870351e1ed4aad5e8dfd2a63b58c47d8ad
osuossu8 <osuosuossu18@gmail.com>
2022-06-28 19:04:06 +0000

    bm25 svd vector

---
+++ src/create_bm25_svd_vector_features_text.py
@@ -0,0 +1,176 @@
+import os
+import gc
+import re
+import sys
+sys.path.append("/root/workspace/Foursquare2022")
+import json
+import time
+import math
+import string
+import pickle
+import random
+import joblib
+import itertools
+import warnings
+warnings.filterwarnings("ignore")
+
+import librosa
+import scipy as sp
+import numpy as np
+import pandas as pd
+import soundfile as sf
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+from tqdm.auto import tqdm
+tqdm.pandas()
+
+from contextlib import contextmanager
+from pathlib import Path
+from typing import List
+from typing import Optional
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.model_selection import StratifiedKFold, GroupKFold, KFold
+
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+import torch.nn.functional as F
+from torch.optim import Adam, SGD, AdamW
+from torch.utils.data import DataLoader, Dataset
+
+import transformers
+
+
+import Levenshtein
+import difflib
+import multiprocessing
+from collections import Counter
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+
+from src.machine_learning_util import set_seed, set_device, init_logger, AverageMeter, to_pickle, unpickle
+
+# https://www.kaggle.com/code/guoyonfan/training-data-for-binary-lgb-baseline-0-834/notebook
+
+## Parameters
+is_debug = False
+SEED = 2022
+num_neighbors = 20
+num_split = 5
+feat_columns = ['name', 'address', 'city', 
+            'state', 'zip', 'url', 
+           'phone', 'categories', 'country']
+vec_columns = ['name', 'categories', 'address', 
+               'state', 'url', 'country']
+
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    
+seed_everything(SEED)
+
+
+import nltk
+import re
+import scipy as sp
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+from sklearn.feature_extraction.text import _document_frequency
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+
+class BM25Transformer(BaseEstimator, TransformerMixin):
+    def __init__(self, use_idf=True, k1=2.0, b=0.75):
+        self.use_idf = use_idf
+        self.k1 = k1
+        self.b = b
+
+    def fit(self, X):
+        if not sp.sparse.issparse(X):
+            X = sp.sparse.csc_matrix(X)
+        if self.use_idf:
+            n_samples, n_features = X.shape
+            df = _document_frequency(X)
+            idf = np.log((n_samples - df + 0.5) / (df + 0.5))
+            self._idf_diag = sp.sparse.spdiags(idf, diags=0, m=n_features, n=n_features)
+
+        doc_len = X.sum(axis=1)
+        self._average_document_len = np.average(doc_len)
+
+        return self
+
+    def transform(self, X, copy=True):
+        if hasattr(X, 'dtype') and np.issubdtype(X.dtype, np.float):
+            X = sp.sparse.csr_matrix(X, copy=copy)
+        else:
+            X = sp.sparse.csr_matrix(X, dtype=np.float, copy=copy)
+
+        n_samples, n_features = X.shape
+        doc_len = X.sum(axis=1)
+        sz = X.indptr[1:] - X.indptr[0:-1]
+        rep = np.repeat(np.asarray(doc_len), sz)
+
+        nom = self.k1 + 1
+        denom = X.data + self.k1 * (1 - self.b + self.b * rep / self._average_document_len)
+        data = X.data * nom / denom
+
+        X = sp.sparse.csr_matrix((data, X.indices, X.indptr), shape=X.shape)
+
+        if self.use_idf:
+            check_is_fitted(self, '_idf_diag', 'idf vector is not fitted')
+
+            expected_n_features = self._idf_diag.shape[0]
+            if n_features != expected_n_features:
+                raise ValueError("Input has n_features=%d while the model"
+                                 " has been trained with n_features=%d" % (
+                                     n_features, expected_n_features))
+            X = X * self._idf_diag
+
+        return X 
+
+
+data = pd.read_csv('input/train.csv')
+data['text'] = ''
+for v in vec_columns:
+    data['text'] += data[v].fillna('nan') + ' '
+
+## Data split
+kf = GroupKFold(n_splits=2)
+for i, (trn_idx, val_idx) in enumerate(kf.split(data, 
+                                                data['point_of_interest'], 
+                                                data['point_of_interest'])):
+    data.loc[val_idx, 'set'] = i
+
+valid_data = data[data['set'] == 0]
+train_data = data[data['set'] == 1]
+
+train_ids = train_data['id'].unique().tolist()
+valid_ids = valid_data['id'].unique().tolist()
+
+tv_ids_d = {}
+tv_ids_d['train_ids'] = train_ids
+tv_ids_d['valid_ids'] = valid_ids
+
+pipeline = make_pipeline(
+                        BM25Transformer(use_idf=True, k1=2.0, b=0.75),
+                        TruncatedSVD(n_components=50, random_state=42)
+                    )
+
+for idx in ['train_ids', 'valid_ids']:
+    data2 = data.set_index('id')
+    data2 = data2.loc[tv_ids_d[idx]]
+    data2 = data2.reset_index()
+
+    bm25_svd_vec = pipeline.fit_transform(data2['text'])
+    print(bm25_svd_vec.shape)
+
+    id_2_text_bm25_svd_vector = {k:v for k, v in zip(data2['id'], bm25_svd_vec)}
+
+    to_pickle(f'features/id_2_text_bm25_svd_vector_50d_{idx}.pkl', id_2_text_bm25_svd_vector)
+
